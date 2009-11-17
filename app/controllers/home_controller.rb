091814ccc9e2036110582ca0da1df7219a13b0ad
fsbrainstorm <fsbrainstorm@gmail.com>
2009-11-17 08:42:38 -0500

    Tour pages and some more

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,18 @@
 class HomeController < ApplicationController
+
+  layout "application"
+
   def index
+    render :layout => "home"
+  end
+
+  def tour
+  end
+
+  def usage
+  end
+
+  def privacy
   end
 
 end
