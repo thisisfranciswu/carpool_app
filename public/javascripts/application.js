a77e34c5da4a01a70e2ce3b80738ab7fbebcf153
fsbrainstorm <fsbrainstorm@gmail.com>
2009-11-22 23:24:18 -0500

    Tentative fancy UI for homepage.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,22 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(function(){
+
+  // Homeage: Search for Carpool
+  $("#home_controller.index #pitch div.try form button").click(function() {
+    $("#pitch").children().animate({
+      opacity: 0.01
+    }).parent().slideUp();
+    $("#search_results").append($("#pitch div.try"));
+    $("#map").animate({
+      height: "520px"
+    });
+    $("#search_results").animate({
+      width: "238px"
+    });
+    $("#search_results div.try").animate({
+      opacity: 1
+    });
+    $("#pitch div.try").replaceWith("");
+    return false;
+  });
+
+});
