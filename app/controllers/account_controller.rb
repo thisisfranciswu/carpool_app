56295bd11f3a649e6d6bef7881630098e82419b0
fsbrainstorm <fsbrainstorm@gmail.com>
2009-11-25 01:34:46 -0500

    Added some missing files.

---
+++ app/controllers/account_controller.rb
@@ -0,0 +1,9 @@
+class AccountController < ApplicationController
+
+  def profile
+  end
+
+  def access
+  end
+
+end
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,18 @@
+class PagesController < ApplicationController
+
+  def about
+  end
+
+  def privacy
+  end
+
+  def usage
+  end
+
+  def contact
+  end
+
+  def help
+  end
+
+end
