602fe859c01cfea26f43cb6b625fb9702026fd54
Francis Wu <franciswu@Francis-Wus-MacBook-Pro.local>
2009-12-11 17:48:02 -0500

    Long time no commit

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091115221058) do
+ActiveRecord::Schema.define(:version => 20091206222133) do
+
+  create_table "locations", :force => true do |t|
+    t.string   "name"
+    t.string   "address"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "login",                     :limit => 40
