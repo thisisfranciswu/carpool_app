2de2476b5bb321663ee58924b8f4ba6a97e507b5
fsbrainstorm <fsbrainstorm@gmail.com>
2009-12-25 23:42:54 -0500

    Improved UI for location finder.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -26,48 +26,63 @@ $(function(){
 
   // Find Location
   $("div.location_finder button").live("click", function() {
-    $("div.location_search").hide();
-    $("div.location_select").show();
-    $("div.location_suggestions").html("");
-    JSON_url = "http://maps.google.com/maps/geo?q=" + $("#address_query").val() +
+    init_location_finder($(this));
+    location_suggestions.html("");
+    JSON_url = "http://maps.google.com/maps/geo?q=" + address_query.val() +
                "&key=ABQIAAAAQEr9TuV_HM_RESkrFixrbBQ2zmKkM91hfvLpqxmsruD80EpYvxRJ04C0a46ORhfmQMz7blpad7gahA" +
                "&sensor=false" + "&output=json" + "&callback=?";
     $.getJSON(JSON_url, function(data) {
-      jQuery.each(data.Placemark, function (i, location) {
-        if (location["AddressDetails"]["Accuracy"] > 6) {
-          $("div.location_suggestions").append("<a href='#' title='Select this address' class='select_location'>" + location["address"] + "</a>");
+      if (data.Status["code"] == 200) {
+        location_suggestions_count = 0;
+        jQuery.each(data.Placemark, function (i, location) {
+          if (location["AddressDetails"]["Accuracy"] > 6) {
+            location_suggestions.append("<a href='' title='Select this address' class='select_location'>" + location["address"] + "</a>");
+            location_suggestions_count++;
+          }
+        });
+        if (location_suggestions_count > 0) {
+          location_search.removeClass("err");
+          location_search.hide();
+          location_select.show();
+        } else {
+          location_search.addClass("err");
         }
-      });
+      } else {
+        location_search.addClass("err");
+      }
     });
     return false;
   });
   // Select Location
   $("div.location_finder div.location_suggestions a").live("click", function() {
+    init_location_finder($(this));
     address = $(this).html();
-    $("div.location_select").hide();
-    $("div.location_address").show();
-    $("div.location_address span.value").html(address);
-    $("#address").val(address);
+    location_select.hide();
+    location_address.show();
+    location_address.find("span.value").html(address);
+    location_address.find("input[type=hidden]").val(address);
     return false;
   });
   // Try again
   $("div.location_finder p.try_again a").live("click", function() {
-    $("div.location_select").hide();
-    $("div.location_search").show();
+    init_location_finder($(this));
+    location_select.hide();
+    location_search.show();
     return false;
   });
   // Find another
   $("div.location_address label.description a").live("click", function() {
-    $("div.location_address").hide();
-    $("div.location_search").show();
-    $("div.location_search span.cancel").show();
+    init_location_finder($(this));
+    location_address.hide();
+    location_search.show();
+    location_search.find("span.cancel").show();
     return false;
   });
   // Cancel find another
   $("div.location_search span.cancel a").live("click", function() {
-    $("div.location_search").hide();
-    $("div.location_search span.cancel").hide();
-    $("div.location_address").show();
+    location_search.hide();
+    location_search.find("span.cancel").hide();
+    location_address.show();
   });
   // Save and close modal window
   $("#add_a_location #save_location").live("click", function() {
@@ -79,3 +94,12 @@ $(function(){
 $().ready(function() {
   $("div.modal_window").jqm();
 });
+
+function init_location_finder(e) {
+  this_location_finder = $(e).parents("div.location_finder")
+  location_search = this_location_finder.find("div.location_search");
+  location_select = this_location_finder.find("div.location_select");
+  location_suggestions = this_location_finder.find("div.location_suggestions");
+  location_address = this_location_finder.find("div.location_address");
+  address_query = this_location_finder.find("input.address_query");
+}
